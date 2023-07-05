875771cad7ed980b4cea3765c8803e259d7b0311
Angeth-Herjok <angethherjok@gmail.com>
2023-07-06 00:18:39 +0300

    fisrt commit

---
+++ app/src/main/java/com/example/twittercolon/TweetsRvAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.twittercolon
+
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.twittercolon.databinding.TweetListItemBinding
+
+class TweetsRvAdapter {
+}
+class TweetsViewHolder(binding: TweetListItemBinding):ViewHolder(binding){
+
+}
---
+++ app/src/test/java/TweetData.kt
@@ -0,0 +1,11 @@
+class TweetData {
+}
+data class TweetData(
+    var avatar:String,
+    var displayName:String,
+    var handle:String,
+    var tweet:String,
+    var commentCount:Int,
+    var rtCount:String,
+    var tvCount:Int,
+)
